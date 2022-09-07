98088b095ad1191a20b8fef54e45d1eeb4a4162a
Coin Data School <coindataschool@gmail.com>
2022-09-07 09:16:01 +0900

    rename and add checks for normality and cointegration

---
+++ helper/check_assumptions.py
@@ -0,0 +1,93 @@
+from statsmodels.tsa.stattools import coint, adfuller
+from statsmodels.stats.stattools import jarque_bera
+import pandas as pd
+
+def check_for_stationarity(x: pd.Series, sig_level=0.01):
+    """
+    Check if a time series is stationary.
+    H0: unit root exists (non-stationary). 
+    So if the p-value is less than `sig_level`, we'd reject H0 and conclude the 
+    series is stationary.
+
+    Parameters
+    ----------
+    x : Series
+    sig_level : float
+        Significance level below which we'd conclude statistical significance.
+    """
+
+    pval = adfuller(x)[1]
+    if pval < sig_level:
+        print('p-value = ' + str(pval) + ' The series ' + x.name +
+              ' is likely stationary.')
+        return True
+    else:
+        print('p-value = ' + str(pval) + ' The series ' + x.name +
+              ' is likely non-stationary.')
+        return False
+
+def check_for_normality(x: pd.Series, sig_level=0.01):
+    """
+    Check if a time series is normally distributed.
+    H0: not normal.
+    So if the p-value is less than `sig_level`, we'd reject H0 and conclude the 
+    series is normally distributed.
+
+    Parameters
+    ----------
+    x : Series
+    sig_level : float
+        Significance level below which we'd conclude statistical significance.
+    """
+
+    pval = jarque_bera(x)[1]
+    if pval < sig_level:
+        print('p-value = ' + str(pval) + ' The series ' + x.name +
+              ' is likely normally distributed.')
+        return True
+    else:
+        print('p-value = ' + str(pval) + ' The series ' + x.name +
+              ' is likely not normally distributed.')
+        return False
+
+def check_for_coint(x1: pd.Series, x2: pd.Series, sig_level=0.01):
+    """
+    Check if two time series are cointegrated.
+    H0: not cointegrated.
+    So if the p-value is less than `sig_level`, we'd reject H0 and conclude the 
+    two series are cointegrated.
+
+    Parameters
+    ----------
+    x1 : Series
+    x2 : Series
+    sig_level : float
+        Significance level below which we'd conclude statistical significance.
+    """
+
+    pval = coint(x1, x2)[1]
+    if pval < sig_level:
+        print('p-value = ' + str(pval) + ' The two series ' + x1.name + 
+              ' and ' + x2.name +' are likely cointegrated.')
+        return True
+    else:
+        print('p-value = ' + str(pval) + ' The two series ' + x1.name + 
+              ' and ' + x2.name +' are likely not cointegrated.')
+        return False
+
+# # test
+# T = 100
+# 
+# A = pd.Series(index=range(T))
+# A.name = 'A'
+#
+# B = pd.Series(index=range(T))
+# B.name = 'B'
+#
+# for t in range(T):
+#       A[t] = np.random.normal(0, 1)
+#       B[t] = np.random.normal(np.sin(t), 1)
+#
+# check_for_stationarity(A)
+# check_for_normality(A, 0.05)
+# check_for_coint(A, B)
