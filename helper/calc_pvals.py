55222161a51e94c22e9adc51cf8757e2fa4d5890
Coin Data School <coindataschool@gmail.com>
2022-09-09 10:45:40 +0900

    add pearson and spearman correlation pval

--- helper/calc_pvals.py
+++ helper/calc_pvals.py
@@ -3,6 +3,7 @@ import numpy as np
 import pandas as pd
 from statsmodels.tsa.stattools import coint, adfuller
 from statsmodels.stats.stattools import jarque_bera
+from scipy.stats import pearsonr, spearmanr
 
 def coint_pval(
     s1: Union[np.ndarray, np.array, pd.Series], 
@@ -17,3 +18,15 @@ def stationarity_pval(s: Union[np.ndarray, np.array, pd.Series]):
 def normality_pval(s: Union[np.ndarray, np.array, pd.Series]):
     """ Test if a series is normally distributed and returns the p-value. """
     return jarque_bera(s)[1]
+
+def pearsonr_pval(
+    s1: Union[np.ndarray, np.array, pd.Series], 
+    s2: Union[np.ndarray, np.array, pd.Series]):
+    """ Test if two series are correlated (pearson) and returns the p-value. """
+    return pearsonr(s1, s2)[1] # the 0th element is the pearson correlation
+
+def spearmanr_pval(
+    s1: Union[np.ndarray, np.array, pd.Series], 
+    s2: Union[np.ndarray, np.array, pd.Series]):
+    """ Test if two series are correlated (spearman) and returns the p-value. """
+    return spearmanr(s1, s2)[1] # the 0th element is the spearman correlation
